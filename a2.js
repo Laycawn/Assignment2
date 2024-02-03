b6a9522e308e3429812e1140c76306293e5079e7
Laycawn <olalekan36@gmail.com>
2024-02-03 18:16:49 -0500

    first commit

---
+++ a2.js
@@ -0,0 +1,31 @@
+/*********************************************************************************
+*  WEB700 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Olalekan Yusau Jimoh Student ID:156555229 Date:02-03-2024
+*
+********************************************************************************/ 
+
+
+const collegeData = require('./modules/collegeData');
+
+async function run() {
+  try {
+    await collegeData.initialize();
+    console.log("Initialization successful");
+    const students = await collegeData.getAllStudents();
+    console.log(`Successfully retrieved ${students.length} students`);
+    const courses = await collegeData.getCourses();
+    console.log(`Successfully retrieved ${courses.length} courses`);
+    const tas = await collegeData.getTAs();
+    console.log(`Successfully retrieved ${tas.length} TAs`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
+
+
